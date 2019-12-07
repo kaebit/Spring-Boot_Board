c03af62dcc742049acfef2b12bf2c9e74d16d74b
kaebit <kaibee@kakao.com>
2019-12-08 05:53:39 +0900

    feat: Add error handler about board

---
+++ src/main/java/com/kaebit/boardbackend/Exception/ForbiddenException.java
@@ -0,0 +1,10 @@
+package com.kaebit.boardbackend.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException() { super("Forbidden"); }
+}
