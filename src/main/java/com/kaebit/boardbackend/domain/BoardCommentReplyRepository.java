c66fa92da92a3a36f4ecd9f5b3be3dd2f0a4cd0d
kaebit <kaibee@kakao.com>
2019-12-22 23:00:57 +0900

    feat: update boardComment replies

--- src/main/java/com/kaebit/boardbackend/domain/BoardCommentReplyRepository.java
+++ src/main/java/com/kaebit/boardbackend/domain/BoardCommentReplyRepository.java
@@ -1,11 +1,20 @@
 package com.kaebit.boardbackend.domain;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface BoardCommentReplyRepository extends CrudRepository<BoardCommentReply, Integer> {
 
     @Override
     <S extends BoardCommentReply> S save(S s);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE board_comment_reply SET content = :content WHERE id = :id", nativeQuery = true)
+    void updatedById(Integer id, String content);
 }
